d6644fa8259c72a83944a0aa4499c6ddad7ab34d
mitang31 <mitang31@abv.bg>
2020-03-02 13:39:23 +0200

    Creating the class Person
    This File includes all methods,constructors, getters and setters for the class Person

---
+++ Week 0/Person.cpp
@@ -0,0 +1,17 @@
+class Person{
+   char* name;
+   int age;
+public:
+   Person()
+   ~Person()
+   Person &operator=(const Person& other);
+   void setName(char* newName);
+   void setAge(int newAge);
+   char* getName();
+   int getAge();
+}
+int main(){
+
+
+return 0;
+}
